436ed737c55df56e815d67cd22d3a20bd72e23f9
davilux <103956666+davilux@users.noreply.github.com>
2023-04-13 15:12:33 -0600

    Removed body padding

--- src/styles/Global.styles.js
+++ src/styles/Global.styles.js
@@ -3,8 +3,11 @@ import { createGlobalStyle, ThemeProvider } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   html{
     height:100%;
+    font-size:14px;
+  }
+
+  body{
     margin:0;
     padding:0;
-    font-size:14px;
   }
 `;
