d74fae67b8d8b3668f197eda3cdfc371e69d7379
davilux <103956666+davilux@users.noreply.github.com>
2023-06-09 13:44:02 -0600

    Navbar styled

--- src/styles/Global.styles.js
+++ src/styles/Global.styles.js
@@ -4,10 +4,19 @@ export const GlobalStyle = createGlobalStyle`
   html{
     height:100%;
     font-size:14px;
+    font-family:Montserrat;
+    font-weight:400;
   }
 
   body{
     margin:0;
     padding:0;
   }
+
+  /* CUSTOM COLORS */
+--eerie-black: #172121ff;
+--magnolia: #f6f2ffff;
+--dark-cyan: #32908fff;
+--pomp-and-power: #8d5a97ff;
+--rich-black: #121420ff;
 `;
