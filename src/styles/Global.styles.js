498d7122cd992fdbb297ae8ec1a33d52f267f56b
davilux <103956666+davilux@users.noreply.github.com>
2023-06-09 13:56:41 -0600

    all products styled

--- src/styles/Global.styles.js
+++ src/styles/Global.styles.js
@@ -6,17 +6,11 @@ export const GlobalStyle = createGlobalStyle`
     font-size:14px;
     font-family:Montserrat;
     font-weight:400;
+    color: #0A0A0A;
   }
 
   body{
     margin:0;
     padding:0;
   }
-
-  /* CUSTOM COLORS */
---eerie-black: #172121ff;
---magnolia: #f6f2ffff;
---dark-cyan: #32908fff;
---pomp-and-power: #8d5a97ff;
---rich-black: #121420ff;
 `;
