a3002b09d18915358d2b1eb5704b8193000d93c1
davilux <103956666+davilux@users.noreply.github.com>
2023-06-09 15:55:31 -0600

    Icon added for password visibility on login form.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,61 +1,73 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { loginUser } from '../redux/reducers/usersSlice'
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../redux/reducers/usersSlice";
+import { useNavigate } from "react-router-dom";
+
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 
 const Login = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordHidden, setPasswordHidden] = useState(true)
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordHidden, setPasswordHidden] = useState(true);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleSubmit = async (e) =>  {
-    e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
     //TODO: Improve UI for front-end validation
-    if(!username) return alert('Username required.')
-    if(!password) return alert('Please enter a password.')
-    if(password.length < 8) return alert('Password must be at least 8 characters.')
+    if (!username) return alert("Username required.");
+    if (!password) return alert("Please enter a password.");
+    if (password.length < 8)
+      return alert("Password must be at least 8 characters.");
     else {
-      const dispatchRes = await dispatch(loginUser({
-        username: username.toLowerCase(),
-        password
-      }))
+      const dispatchRes = await dispatch(
+        loginUser({
+          username: username.toLowerCase(),
+          password,
+        })
+      );
 
       // Check if a user successfully logged in:
-      if(dispatchRes.payload.refreshToken) navigate('/shop')
+      if (dispatchRes.payload.refreshToken) navigate("/shop");
     }
-  }
+  };
 
   const toggleHiddenPassword = (e) => {
-    e.preventDefault()
-    setPasswordHidden(!passwordHidden)
-  }
+    e.preventDefault();
+    setPasswordHidden(!passwordHidden);
+  };
 
   return (
     <>
-      <h1>
-        Login
-      </h1>
-      <form onSubmit={(e)=>handleSubmit(e)}>
+      <h1>Login</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <label>
           Username:
-          <input type="text" name="username" onChange={(event)=> setUsername(event.target.value)}/>
+          <input
+            type="text"
+            name="username"
+            onChange={(event) => setUsername(event.target.value)}
+          />
         </label>
         <label>
           Password:
-          <input type={passwordHidden ? 'password' : 'text'} password="password" onChange={(event)=> setPassword(event.target.value)}/>
+          <input
+            type={passwordHidden ? "password" : "text"}
+            password="password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button onClick={toggleHiddenPassword}>
+            {passwordHidden ? <VisibilityIcon /> : <VisibilityOffIcon />}
+          </button>
         </label>
-        <input type="submit" value="Submit"/>
+        <input type="submit" value="Submit" />
       </form>
 
-
-        {/* TODO: replace the following button with an icon */}
-        <button onClick={toggleHiddenPassword}>show/hide password</button>
+      {/* TODO: replace the following button with an icon */}
     </>
-  )
-}
+  );
+};
 
-export default Login
-
+export default Login;
